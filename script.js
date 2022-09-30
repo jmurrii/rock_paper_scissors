95b157ab66bbe27d5681fc4cbd967111b5d904df
MAC <mac@MURRII-MacBook-Pro.local>
2022-09-30 07:03:35 +0100

    created setUserChoice function

--- script.js
+++ script.js
@@ -1,40 +1,98 @@
-async function getUserChoice() {
+const choicesArray = ["ROCK", "PAPER", "SCISSORS"];
+let userChoice;
+
+
+
+
+function setUserChoice() {
 
     const rock = document.getElementById("rock");
 
     rock.addEventListener("click", function () {
         document.getElementById("userChoice").innerText = "ROCK";
+        userChoosesRock();
+        gameLoop();
     })
+
 
     const paper = document.getElementById("paper");
 
     paper.addEventListener("click", function () {
         document.getElementById("userChoice").innerText = "PAPER";
+        userChoosesPaper();
+        gameLoop();
     })
 
     const scissors = document.getElementById("scissors");
 
     scissors.addEventListener("click", function () {
         document.getElementById("userChoice").innerText = "SCISSORS";
+        userChoosesScissors();
+        gameLoop();
     })
 
+};
+
+setUserChoice();
+
+function userChoosesRock() {
+    userChoice = choicesArray[0];
+    console.log("Challenger's choice is " + userChoice);
+    return userChoice
+}
+function userChoosesPaper() {
+    userChoice = choicesArray[1];
+    console.log("Challenger's choice is " + userChoice);
+    return userChoice
+}
+function userChoosesScissors() {
+    userChoice = choicesArray[2];
+    console.log("Challenger's choice is " + userChoice);
+    return userChoice
 }
 
-getUserChoice();
+
+// setTimeout(() => {
+//     console.log(setUserChoice() + "!!");
+// }, 5000)
 
 
-/**** ****  getComputerChoice   ****  ****/
+function getComputerChoice() {
+    let i = Math.floor(Math.random() * choicesArray.length);
 
-async function getComputerChoice() {
-    await getUserChoice();
-    let gameArray = ["ROCK", "PAPER", "SCISSORS"];
-
-    let i = Math.floor(Math.random() * 3);
-    // console.log(gameArray[i]);
-
-    document.getElementById("computerChoice").innerText = gameArray[i];
+    document.getElementById("computerChoice").innerText = choicesArray[i];
+    console.log('Computer\'s choice is ' + choicesArray[i]);
+    return choicesArray[i];
 }
 
-getComputerChoice();
+
+// function compare
+
+
+
+function gameLoop() {
+    setTimeout(getComputerChoice, 2000);
+    // compare scores
+    //alert who wins
+    // 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
